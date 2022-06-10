228e080ae4b7890d369f73bc18fcabd722dbae2f
Jonathan Henderson <hendejon@Jonathans-MacBook-Pro.local>
2022-06-10 12:02:48 -0500

    Add serde feature

--- src/status.rs
+++ src/status.rs
@@ -1,3 +1,8 @@
+#[cfg_attr(
+    feature = "serde",
+    derive(serde::Deserialize, serde::Serialize),
+    serde(rename_all = "lowercase")
+)]
 #[derive(Debug, Clone, Copy)]
 pub enum Status {
     Running,
