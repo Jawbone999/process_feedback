ec8cc9e01fddd4bdd463c35a257b9d3cd3d59276
Jonathan Henderson <jkhenderson999@gmail.com>
2022-06-13 12:55:42 -0500

    Add fn

--- src/result.rs
+++ src/result.rs
@@ -1,13 +1,15 @@
 pub type ResultFeedback<T, E> = Result<(T, Option<String>), (E, Option<String>)>;
 
 pub trait WithMessage<T, E> {
-    fn with_msg<S: Into<String>>(self, message: S) -> ResultFeedback<T, E>;
+    fn with_msg(self, message: impl Into<String>) -> ResultFeedback<T, E>;
 
     fn no_msg(self) -> ResultFeedback<T, E>;
+
+    fn with_err_msg(self, message: impl Into<String>) -> Result<T, (E, Option<String>)>;
 }
 
 impl<T, E> WithMessage<T, E> for Result<T, E> {
-    fn with_msg<S: Into<String>>(self, message: S) -> ResultFeedback<T, E> {
+    fn with_msg(self, message: impl Into<String>) -> ResultFeedback<T, E> {
         match self {
             Ok(value) => Ok((value, Some(message.into()))),
             Err(error) => Err((error, Some(message.into()))),
@@ -18,6 +20,13 @@ impl<T, E> WithMessage<T, E> for Result<T, E> {
         match self {
             Ok(value) => Ok((value, None)),
             Err(error) => Err((error, None)),
+        }
+    }
+
+    fn with_err_msg(self, message: impl Into<String>) -> Result<T, (E, Option<String>)> {
+        match self {
+            Ok(value) => Ok(value),
+            Err(error) => Err((error, Some(message.into()))),
         }
     }
 }
