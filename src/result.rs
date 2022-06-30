84635708df3c5564cbe5cda18ca932ae505ed0d1
Jonathan Henderson <jkhenderson999@gmail.com>
2022-06-30 17:26:50 -0500

    Add message to async_task and rename result method

--- src/result.rs
+++ src/result.rs
@@ -7,7 +7,7 @@ pub trait WithMessage<T, E> {
 
     fn with_err_msg(self, message: impl Into<String>) -> Result<T, (E, Option<String>)>;
 
-    fn err_no_msg(self) -> Result<T, (E, Option<String>)>;
+    fn no_err_msg(self) -> Result<T, (E, Option<String>)>;
 }
 
 impl<T, E> WithMessage<T, E> for Result<T, E> {
@@ -32,7 +32,7 @@ impl<T, E> WithMessage<T, E> for Result<T, E> {
         }
     }
 
-    fn err_no_msg(self) -> Result<T, (E, Option<String>)> {
+    fn no_err_msg(self) -> Result<T, (E, Option<String>)> {
         match self {
             Ok(value) => Ok(value),
             Err(error) => Err((error, None)),
--- src/task.rs
+++ src/task.rs
@@ -3,13 +3,13 @@ use crate::ResultFeedback;
 use crate::Status;
 
 #[cfg(feature = "sync")]
-pub fn task<S, M, F, T, E>(name: S, messenger: &M, func: F) -> Result<T, E>
+pub fn task<S, M, F, T, E>(name: S, message: Option<String>, messenger: &M, func: F) -> Result<T, E>
 where
     S: AsRef<str>,
     M: Messenger,
     F: FnOnce() -> ResultFeedback<T, E>,
 {
-    messenger.send(name.as_ref(), Status::Running, None);
+    messenger.send(name.as_ref(), Status::Running, message);
 
     let (result, msg) = match func() {
         Ok((value, msg)) => (Ok(value), msg),
@@ -24,13 +24,20 @@ where
 }
 
 #[cfg(feature = "async")]
-pub async fn async_task<S, M, F, T, E>(name: S, messenger: &M, future: F) -> Result<T, E>
+pub async fn async_task<S, M, F, T, E>(
+    name: S,
+    message: Option<String>,
+    messenger: &M,
+    future: F,
+) -> Result<T, E>
 where
     S: AsRef<str>,
     M: AsyncMessenger,
     F: std::future::Future<Output = ResultFeedback<T, E>>,
 {
-    messenger.send(name.as_ref(), Status::Running, None).await;
+    messenger
+        .send(name.as_ref(), Status::Running, message)
+        .await;
 
     let (result, msg) = match future.await {
         Ok((value, msg)) => (Ok(value), msg),
