a6a9eab9a7a28f2e6c47f9bccae1aeeaced3240d
Jonathan Henderson <jkhenderson999@gmail.com>
2022-07-06 11:18:15 -0500

    dummy messenger explicit

--- src/messenger.rs
+++ src/messenger.rs
@@ -12,12 +12,14 @@ pub trait AsyncMessenger {
     async fn send(&self, task_name: &str, status: Status, message: Option<String>);
 }
 
-impl Messenger for () {
+pub struct DummyMessenger;
+
+impl Messenger for DummyMessenger {
     fn send(&self, task_name: &str, status: Status, message: Option<String>) {}
 }
 
 #[cfg(feature = "async")]
 #[async_trait::async_trait]
-impl AsyncMessenger for () {
+impl AsyncMessenger for DummyMessenger {
     async fn send(&self, task_name: &str, status: Status, message: Option<String>) {}
 }
